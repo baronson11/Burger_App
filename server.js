909bb313a07c4eec23e424713d7070e98bc19c68
smibbie <35998407+smibbie@users.noreply.github.com>
2018-08-28 20:25:48 -0400

    debugging post & put routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
-const routes = require("./controllers/controller.js");
 
 // PORT --------------------------------------------------
 const PORT = process.env.PORT || 3000;
@@ -22,7 +21,23 @@ app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Main Router --------------------------------------------
+const routes = require("./controllers/controller.js");
+
 app.use(routes);
+
+// Cross Origin Resource Sharing ----------------------------------------------
+// Set up for API usage permissions -------------------------------------------
+// NOTE: This must be set up in order for your API to be used by a web browser.
+// ----------------------------------------------------------------------------
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Error Handling -----------------------------------------
 app.use((req, res, next) => {
